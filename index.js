a85065bf6503491c56d187e1b9b8d97d799b8046
smh1988-2 <seamhurley1988@gmail.com>
2021-11-07 13:05:45 -0800

    commit tree 9011f359bbbcee8c272ca22a8d2619cef88f8ff5

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
 const farmAnimals = 'cow horse sheep pig chicken';
 
+const moo = farmAnimals.split(" ")[0];
+const neigh = farmAnimals.split(" ")[1];
+const baa = farmAnimals.split(" ")[2];
+const oink = farmAnimals.split(" ")[3];
+const cluck = farmAnimals.split(" ")[4];
+
+const bessie = farmAnimals.split(" ")[0];
+const dolly = farmAnimals.split(" ")[2];
+const babe = farmAnimals.split(" ")[3];
+const little = farmAnimals.split(" ")[4];
+
+const blackAndWhite = farmAnimals.split(" ")[0];
+const black = farmAnimals.split(" ")[2];
+const pink = farmAnimals.split(" ")[3];
+
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+const red = colors[0];
+const orange = colors[1];
+const yellow = colors[2];
+const green = colors[3];
+const blue = colors[4];
+const indigo = colors[5];
+const violet = colors[6];
+
+const r = colors[0];
+const o = colors[1];
+const y = colors[2];
+const g = colors[3];
+const b = colors[4];
+const i = colors[5];
+const v = colors[6];
+
+const indg = colors[5];
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -9,6 +42,13 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+
+const muppetName = muppet.muppetName;
+const color = muppet.color;
+const song = muppet.song;
+const job = muppet.job;
+const partner = muppet.partner;
+
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -24,6 +64,11 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+
+const song2 = nestedMuppet.album.theMuppetMovie.song2;
+const song4 = nestedMuppet.album.theMuppetMovie.song4;
+const nestedJob = nestedMuppet.nestedJob;
+const nestedPartner = nestedMuppet.nestedPartner;
 
 // Strings
 
